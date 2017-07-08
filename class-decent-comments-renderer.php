b1d49f2dfd792943d2082b8ee4282d349f88b46a
Jon007 <jonathan.moore@bcs.org>
2017-07-09 05:58:03 +0800

    remove hardcode avatar size

--- class-decent-comments-renderer.php
+++ class-decent-comments-renderer.php
@@ -315,7 +315,6 @@ class Decent_Comments_Renderer {
 			if ( isset( $options['avatar_size'] ) ) {
 				$avatar_size = intval( $options['avatar_size'] );
 			}
-			$options['avatar_size']=80;
 			if ( isset( $options['excerpt'] ) ) {
 				$excerpt = ( $options['excerpt'] !== 'false' && $options['excerpt'] !== false );
 			}
