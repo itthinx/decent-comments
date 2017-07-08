5c54dfbd47760ba351bf99053b5f82f07eec4404
itthinx <itthinx@gmail.com>
2017-07-08 18:16:25 +0200

    formatting

--- class-decent-comment.php
+++ class-decent-comment.php
@@ -108,7 +108,7 @@ class Decent_Comment {
 		extract( $this->query_vars, EXTR_SKIP );
 
 		// $args can be whatever, only use the args defined in defaults to compute the key
-		$key = md5( serialize( compact(array_keys($defaults)) )  );
+		$key = md5( serialize( compact( array_keys( $defaults ) ) )  );
 		$last_changed = wp_cache_get( 'last_changed', 'comment' );
 		if ( !$last_changed ) {
 			$last_changed = time();
