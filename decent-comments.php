303bdf211ca1e58bcb01dcef832b45bc7151c6e9
itthinx <itthinx@itthinx.com>
2023-12-28 11:21:49 +0100

    version updates

--- decent-comments.php
+++ decent-comments.php
@@ -4,6 +4,8 @@
  * Plugin URI: https://www.itthinx.com/plugins/decent-comments
  * Description: Provides configurable means to display comments that include author's avatars, author link, link to post and most importantly an excerpt of each comment. Thanks for supporting our work with a purchase in our <a href="https://www.itthinx.com/shop/">Shop</a>!
  * Version: 1.13.0
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
  * Author: itthinx
  * Author URI: https://www.itthinx.com
  * Donate-Link: https://www.itthinx.com/shop/
@@ -11,7 +13,7 @@
  * Domain Path: /languages
  * License: GPLv3
  *
- * Copyright (c) 2015 - 2022 "kento" Karim Rahimpur www.itthinx.com
+ * Copyright (c) 2015 - 2024 "kento" Karim Rahimpur www.itthinx.com
  *
  * This code is released under the GNU General Public License Version 3.
  * The following additional terms apply to all files as per section
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: itthinx
 Donate link: https://www.itthinx.com/shop/
 Tags: comment, comment excerpt, comments, comment shortcode, comment widget, comment excerpts, avatar, comment API, excerpt, excerpts, latest comments, recent comments, shortcode, shortcodes
-Requires at least: 5.6
+Requires at least: 6.0
 Tested up to: 6.4
-Requires PHP: 5.6.0
+Requires PHP: 7.4
 Stable tag: 1.13.0
 License: GPLv3
 
