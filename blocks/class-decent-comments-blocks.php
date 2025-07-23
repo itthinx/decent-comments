69bc67b9fc2e3c0433e728c692ac05b2153d8960
itthinx <itthinx@itthinx.com>
2025-07-23 11:49:35 +0200

    header revised

--- blocks/class-decent-comments-blocks.php
+++ blocks/class-decent-comments-blocks.php
@@ -2,7 +2,7 @@
 /**
  * class-decent-comments-blocks.php
  *
- * Copyright (c) 2011 "kento" Karim Rahimpur www.itthinx.com
+ * Copyright (c) www.itthinx.com
  *
  * This code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt.
